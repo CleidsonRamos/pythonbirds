3b1c15480d19f99b5bf9aff89ad88a8cec1561f8
Cleidson <cleidson.ramosmartins@gmail.com>
2021-04-19 20:47:56 -0300

    Criados atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,19 @@
 class Pessoa():
-    pass
+
+    # metodo é executado quando constroi o objeto
+    def __init__(self, nome = None, idade=31): #nome é um valor nulo
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__=='__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'fulnao'
+    print(p.nome)
+    print(p.idade)
