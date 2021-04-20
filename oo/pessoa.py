643aab54b39319fba51d1a8922d7b1220511c970
Cleidson <cleidson.ramosmartins@gmail.com>
2021-04-20 19:50:27 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,19 @@
 class Pessoa():
 
     # metodo é executado quando constroi o objeto
-    def __init__(self, nome = None, idade=31): #nome é um valor nulo
+    def __init__(self, *filhos, nome = None, idade=31): #nome é um valor nulo
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__=='__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'fulnao'
-    print(p.nome)
-    print(p.idade)
+    cleidson = Pessoa(nome='Cleidson')
+    zeus = Pessoa(cleidson, nome='Zeus')
+
+    print(zeus.nome)
+    print(zeus.idade)
+    for filho in zeus.filhos:
+        print(filho.nome)
